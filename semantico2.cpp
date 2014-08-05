5e133abf41c2a6fbfd463b5bf521e92525175cf6
Daniel Acosta <danyacosta91@gmail.com>
2014-08-04 20:51:40 -0600

    Errores en Amálisis Semántico

---
+++ semantico2.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+
+void f1(){
+    int x;
+    int z;
+}
---
+++ semantico3.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    string ret = 1234;
+    return 0;
+}
