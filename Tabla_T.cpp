2d8c71a9817526b3f90b14d5da531683da89a6bd
Daniel Acosta <danyacosta91@gmail.com>
2014-08-10 21:28:15 -0600

    Tabla de Transición

--- Tabla_T.cpp
+++ Tabla_T.cpp
@@ -5,7 +5,34 @@
 using namespace std;
 
 bool validar( const char * _str, char** _data ){
-	return false;
+	bool _change = false;
+	for (int i = 0; i < strlen( _str ); ++i){
+		if( i == 0 )
+			if( _data[0][0] != _str[i] )
+				return false;
+		else{
+			if( _change == false ){
+				do{
+					i++;
+				}while( _data[0][0] == _str[i] );
+
+				_change = true;
+			}else{
+				if( _data[1][0] != _str[i] )
+					return false;
+
+				do{
+					i++;
+					if( _data[1][0] != _str[i] )
+						return false;
+				}while( _data[1][0] == _str[i] );
+
+				if( _data[1][0] != _str[i] )
+					return false;
+			}
+		}
+	}
+	return true;
 }
 
 int main(){
@@ -13,19 +40,21 @@ int main(){
 	for( int i = 0; i < 2; i++ )
 		_m[i] = new char[2];
 
-	_m[0][0] = 'p';_m[0][1] = 'q';
-	_m[1][0] = 'q';_m[1][1] = ' ';
+	_m[0][0] = '1';_m[0][1] = '0';
+	_m[1][0] = '0';_m[1][1] = ' ';
 
 	string _get = "";
 
 	do{
 		cout << "Ingrese cadena (exit - salir):" << endl;
 		cin >> _get;
-		if( validar( _get.c_str(), _m ) )
-			cout << "Cadena correcta!" << endl;
-		else
-			cout << "Cadena incorrecta!" << endl;
-	}while( strcmp( _get.c_str(), "exit" ) == 1 );
+		if( strcmp( "exit", _get.c_str() ) == 1 ){
+			if( validar( _get.c_str(), _m ) )
+				cout << "Cadena correcta!" << endl;
+			else if( validar( _get.c_str(), _m ) == false )
+				cout << "Cadena incorrecta!" << endl;
+		}
+	}while( strcmp( "exit", _get.c_str() ) == 1 );
 
 	free( _m );
 	return 0;
