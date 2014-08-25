027bef0c44c0f836e40ae935c50c339ed75094f3
Daniel Acosta <danyacosta91@gmail.com>
2014-08-24 19:12:05 -0600

    Lua Script, intergración con C#

--- LuaScript/LuaScript/Program.cs
+++ LuaScript/LuaScript/Program.cs
@@ -12,13 +12,16 @@ namespace LuaScript
         static void Main(string[] args)
         {
             Lua lua = new Lua();
-            lua["num1"] = 2;
-            lua["num2"] = 45;
-            lua["palabra"] = "La suma es: ";
-            double num = (double)lua["num1"];
-            double num2 = (double)lua["num2"];
-            string palabra = (string)lua["palabra"];
-            Console.WriteLine(palabra + (num2 + num));
+            lua["_n1"] = 53;
+            lua["_n2"] = 112;
+            lua["_suma"] = "La suma es: ";
+            lua["_mult"] = "La multiplicacion es: ";
+            double _n1 = (double)lua["_n1"];
+            double _n2 = (double)lua["_n2"];
+            string _suma = (string)lua["_suma"];
+            string _mult = (string)lua["_mult"];
+            Console.WriteLine(_suma + (_n1 + _n2));
+            Console.WriteLine(_mult + (_n1 * _n2));
             Console.ReadLine();
         }
     }
