777afdc9f59bf998e4e46738c5bc027c1ecd57f6
Daniel Acosta <danyacosta91@gmail.com>
2014-08-24 20:12:08 -0600

    Lua Form, intergración con C#

---
+++ LuaForm/LuaForm/Form1.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace LuaForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_ingresar = new System.Windows.Forms.Button();
+            this.txt_text = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btn_ingresar
+            // 
+            this.btn_ingresar.Location = new System.Drawing.Point(106, 227);
+            this.btn_ingresar.Name = "btn_ingresar";
+            this.btn_ingresar.Size = new System.Drawing.Size(75, 23);
+            this.btn_ingresar.TabIndex = 0;
+            this.btn_ingresar.Text = "Ejecutar";
+            this.btn_ingresar.UseVisualStyleBackColor = true;
+            this.btn_ingresar.Click += new System.EventHandler(this.btn_ingresar_Click);
+            // 
+            // txt_text
+            // 
+            this.txt_text.Location = new System.Drawing.Point(13, 13);
+            this.txt_text.Multiline = true;
+            this.txt_text.Name = "txt_text";
+            this.txt_text.Size = new System.Drawing.Size(259, 193);
+            this.txt_text.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.txt_text);
+            this.Controls.Add(this.btn_ingresar);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(300, 300);
+            this.MinimumSize = new System.Drawing.Size(300, 300);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "C#";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_ingresar;
+        private System.Windows.Forms.TextBox txt_text;
+    }
+}
+
---
+++ LuaForm/LuaForm/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using LuaInterface;
+using Lua511;
+using luanet;
+
+namespace LuaForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_ingresar_Click(object sender, EventArgs e)
+        {
+            Lua lua = new Lua();
+            String _ejecutar = txt_text.Text;
+            lua.DoString(_ejecutar);
+        }
+    }
+}
