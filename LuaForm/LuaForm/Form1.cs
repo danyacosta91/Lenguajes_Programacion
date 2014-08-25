92b7e092a4a080ebf58c6c8398b6bd01cf89b7c4
Daniel Acosta <danyacosta91@gmail.com>
2014-08-24 21:22:59 -0600

    Lua Form, intergración con C#

--- LuaForm/LuaForm/Form1.cs
+++ LuaForm/LuaForm/Form1.cs
@@ -7,9 +7,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using luanet;
 using LuaInterface;
 using Lua511;
-using luanet;
+using LuaForm;
 
 namespace LuaForm
 {
