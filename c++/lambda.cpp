d7b1604b652998e2647b7947de66f1576bcbd192
Daniel Acosta <danyacosta91@gmail.com>
2014-08-31 21:50:18 -0600

    Lambda expressions

--- c++/lambda.cpp
+++ c++/lambda.cpp
@@ -1,16 +1,14 @@
 #include <iostream>
 using namespace std;
 
-static Func<int, int> Factorial = x => x < 0 ? -1 : x == 1 || x == 0 ? 1 : x * Factorial(x - 1);
+Factorial<int, int> Factorial = x => x < 0 ? -1 : x == 1 || x == 0 ? 1 : x * Factorial(x - 1);
 
 int main(){
 	int n;
 	cout << "Ingrese n:" << endl;
 	cin >> n;
 
-	
-
-	cout << "Factorial es: " << factorial(n) << endl;
+	cout << "Factorial es: " << Factorial(n) << endl;
 
 	return 0;
 }
