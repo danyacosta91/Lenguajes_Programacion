f32c8e4b4e9f86358adaabd46099f7d669ebabe2
Daniel Acosta <danyacosta91@gmail.com>
2014-08-31 21:30:37 -0600

    Ranged For loops

--- c++/ranged_loop.cpp
+++ c++/ranged_loop.cpp
@@ -27,11 +27,11 @@ public:
 
 int main(){
 	std::vector<Alumno*> v;
-	v.push_back( new Alumno("PEDRO JUAN", 59.3) );
-	v.push_back( new Alumno("CARLOS JOSE", 59.4) );
-	v.push_back( new Alumno("ANA RUTH", 59.2) );
-	v.push_back( new Alumno("CARMEN FIORELLA", 59.5) );
-	v.push_back( new Alumno("PANCRACIA ANASTACIA", 100) );
+	v.push_back( new Alumno("PEDRO JUAN", "pedro.juan@gmail.com", 59.3) );
+	v.push_back( new Alumno("CARLOS JOSE", "carlos.jose@gmail.com", 59.4) );
+	v.push_back( new Alumno("ANA RUTH", "ana.ruth@gmail.com", 59.2) );
+	v.push_back( new Alumno("CARMEN FIORELLA", "carmen.fiorella@gmail.com", 59.5) );
+	v.push_back( new Alumno("PANCRACIA ANASTACIA", "pancracia.anastacia@gmail.com", 100) );
 
 	for( Alumno *i : v )
 		cout << i->toString() << endl;
