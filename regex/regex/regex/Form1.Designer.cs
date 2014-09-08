0a19469e427437d8e07ca2110e45a415f3da06dc
Daniel Acosta <danyacosta91@gmail.com>
2014-09-07 23:29:55 -0600

    REGEX

---
+++ regex/regex/regex/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace regex
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txt_c = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(25, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "String en C:";
+            // 
+            // txt_c
+            // 
+            this.txt_c.Location = new System.Drawing.Point(94, 40);
+            this.txt_c.Name = "txt_c";
+            this.txt_c.Size = new System.Drawing.Size(174, 20);
+            this.txt_c.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(297, 36);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Validar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(458, 355);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txt_c);
+            this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "REGEX";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txt_c;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ regex/regex/regex/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+
+namespace regex
+{
+    public partial class Form1 : Form
+    {
+        #region Variables
+        Regex _cString = new Regex(@"(\""[a-zA-Z0-9,!#$%&/()=?¡\+*-/_.<>\s]*\""$)|(\""[a-zA-Z0-9,!#$%&/()=?¡\+*-/_.<>\s]*\\""[a-zA-Z0-9,!#$%&/()=?¡\+*-/_.<>\s]*\""$)");
+        #endregion
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (_cString.IsMatch(txt_c.Text))
+                MessageBox.Show("Es válida!");
+            else
+                MessageBox.Show("No es válida!");
+        }
+    }
+}
