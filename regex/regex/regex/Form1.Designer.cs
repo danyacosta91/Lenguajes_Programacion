ba056f3c374001b1a6933aa89a4595b4a9235a6f
Daniel Acosta <danyacosta91@gmail.com>
2014-09-07 23:53:58 -0600

    REGEX

--- regex/regex/regex/Form1.Designer.cs
+++ regex/regex/regex/Form1.Designer.cs
@@ -31,12 +31,18 @@
             this.label1 = new System.Windows.Forms.Label();
             this.txt_c = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
+            this.btn_parentesis = new System.Windows.Forms.Button();
+            this.txt_parentesis = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btn_email = new System.Windows.Forms.Button();
+            this.txt_email = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(25, 40);
+            this.label1.Location = new System.Drawing.Point(25, 44);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(62, 13);
             this.label1.TabIndex = 0;
@@ -59,11 +65,69 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // btn_parentesis
+            // 
+            this.btn_parentesis.Location = new System.Drawing.Point(297, 81);
+            this.btn_parentesis.Name = "btn_parentesis";
+            this.btn_parentesis.Size = new System.Drawing.Size(75, 23);
+            this.btn_parentesis.TabIndex = 5;
+            this.btn_parentesis.Text = "Validar";
+            this.btn_parentesis.UseVisualStyleBackColor = true;
+            this.btn_parentesis.Click += new System.EventHandler(this.btn_parentesis_Click);
+            // 
+            // txt_parentesis
+            // 
+            this.txt_parentesis.Location = new System.Drawing.Point(94, 85);
+            this.txt_parentesis.Name = "txt_parentesis";
+            this.txt_parentesis.Size = new System.Drawing.Size(174, 20);
+            this.txt_parentesis.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(25, 89);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Parentesis:";
+            // 
+            // btn_email
+            // 
+            this.btn_email.Location = new System.Drawing.Point(297, 128);
+            this.btn_email.Name = "btn_email";
+            this.btn_email.Size = new System.Drawing.Size(75, 23);
+            this.btn_email.TabIndex = 8;
+            this.btn_email.Text = "Validar";
+            this.btn_email.UseVisualStyleBackColor = true;
+            this.btn_email.Click += new System.EventHandler(this.btn_email_Click);
+            // 
+            // txt_email
+            // 
+            this.txt_email.Location = new System.Drawing.Point(94, 132);
+            this.txt_email.Name = "txt_email";
+            this.txt_email.Size = new System.Drawing.Size(174, 20);
+            this.txt_email.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(25, 136);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Email:";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(458, 355);
+            this.Controls.Add(this.btn_email);
+            this.Controls.Add(this.txt_email);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btn_parentesis);
+            this.Controls.Add(this.txt_parentesis);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.txt_c);
             this.Controls.Add(this.label1);
@@ -82,6 +146,12 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox txt_c;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btn_parentesis;
+        private System.Windows.Forms.TextBox txt_parentesis;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btn_email;
+        private System.Windows.Forms.TextBox txt_email;
+        private System.Windows.Forms.Label label3;
     }
 }
 
--- regex/regex/regex/Form1.cs
+++ regex/regex/regex/Form1.cs
@@ -15,6 +15,8 @@ namespace regex
     {
         #region Variables
         Regex _cString = new Regex(@"(\""[a-zA-Z0-9,!#$%&/()=?¡\+*-/_.<>\s]*\""$)|(\""[a-zA-Z0-9,!#$%&/()=?¡\+*-/_.<>\s]*\\""[a-zA-Z0-9,!#$%&/()=?¡\+*-/_.<>\s]*\""$)");
+        Regex _parentesis = new Regex(@"[a-z0-9\n\s]*\([a-z0-9\n\s]*\)[a-z0-9\n\s]*$");
+        Regex _email = new Regex(@"^[a-z0-9_.]+@[a-z0-9-]+\.[a-z]{2,4}$");
         #endregion
 
         public Form1()
@@ -25,6 +27,22 @@ namespace regex
         private void button1_Click(object sender, EventArgs e)
         {
             if (_cString.IsMatch(txt_c.Text))
+                MessageBox.Show("Es válida!");
+            else
+                MessageBox.Show("No es válida!");
+        }
+
+        private void btn_parentesis_Click(object sender, EventArgs e)
+        {
+            if (_parentesis.IsMatch(txt_parentesis.Text))
+                MessageBox.Show("Es válida!");
+            else
+                MessageBox.Show("No es válida!");
+        }
+
+        private void btn_email_Click(object sender, EventArgs e)
+        {
+            if (_email.IsMatch(txt_email.Text))
                 MessageBox.Show("Es válida!");
             else
                 MessageBox.Show("No es válida!");
