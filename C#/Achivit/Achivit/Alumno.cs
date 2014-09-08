ace84c3ec54b8af1a4ee218db818c906e79a2c40
Daniel Acosta <danyacosta91@gmail.com>
2014-09-07 18:51:19 -0600

    Achivit C#

--- C#/Achivit/Achivit/Alumno.cs
+++ C#/Achivit/Achivit/Alumno.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 
 namespace Achivit
 {
@@ -13,6 +14,10 @@ namespace Achivit
         private string _cuenta;
         private string _email;
         private int _edad;
+        Regex _nombres = new Regex(@"^[A-Za-z][A-Za-zñ,\s-,\s(,\s,\s.)]+$");
+        Regex _numeros = new Regex("^[0-9]+$");
+        Regex _emailR = new Regex(@"^[a-z0-9_.]+@[a-z0-9-]+\.[a-z]{2,4}$");
+        Regex _cuentaR = new Regex(@"^[0-9]{8}$");
         #endregion
 
         #region Constructores
@@ -26,10 +31,10 @@ namespace Achivit
 
         public Alumno(string nombre, string cuenta, string email, int edad)
         {
-            _nombre = nombre;
-            _email = email;
-            _cuenta = cuenta;
-            _edad = edad;
+            Nombre = nombre;
+            Email = email;
+            Cuenta = cuenta;
+            Edad = edad;
         }
         #endregion
 
@@ -37,25 +42,45 @@ namespace Achivit
         public string Nombre
         {
             get { return _nombre; }
-            set { _nombre = value; }
+            set {
+                if (_nombres.IsMatch(value))
+                    _nombre = value;
+                else
+                    throw new Exception("Error al escribir el nombre!");
+            }
         }
 
         public string Cuenta
         {
             get { return _cuenta; }
-            set { _cuenta = value; }
+            set { 
+                if( _cuentaR.IsMatch(value) )
+                    _cuenta = value; 
+                else
+                    throw new Exception("Error al escribir la cuenta!");
+            }
         }
 
         public int Edad
         {
             get { return _edad; }
-            set { _edad = value; }
+            set { 
+                if( _numeros.IsMatch(value.ToString()) )
+                    _edad = value; 
+                else
+                    throw new Exception("Error al escribir la edad!");
+            }
         }
 
         public string Email
         {
             get { return _email; }
-            set { _email = value; }
+            set {
+                if( _emailR.IsMatch(value) )
+                    _email = value; 
+                else
+                    throw new Exception("Error al escribir el correo!");
+            }
         }
         #endregion
 
