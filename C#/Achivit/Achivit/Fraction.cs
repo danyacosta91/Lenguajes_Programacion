c578ac2aed5104f12fa7dfc6bceaf18cd7910623
Daniel Acosta <danyacosta91@gmail.com>
2014-09-07 19:37:36 -0600

    Achivit C#

--- C#/Achivit/Achivit/Fraction.cs
+++ C#/Achivit/Achivit/Fraction.cs
@@ -8,5 +8,39 @@ namespace Achivit
 {
     class Fraction
     {
+        #region Variables
+        private int _num;
+        private int _den;
+        #endregion
+
+        #region Get/Set
+        public int Numerador
+        {
+            get { return _num; }
+            set { _num = value; }
+        }
+
+        public int Denominador
+        {
+            get { return _den; }
+            set { _den = value; }
+        }
+        #endregion
+
+        public Fraction(int numerador, int denominador)
+        {
+            _num = numerador;
+            _den = denominador;
+        }
+
+        public Fraction Multiplicar(Fraction frac)
+        {
+            return new Fraction(frac.Denominador * Numerador, frac.Numerador * Denominador);
+        }
+
+        public static Fraction operator *(Fraction f1, Fraction f2)
+        {
+            return new Fraction(f1.Denominador * f2.Numerador, f1.Numerador * f2.Denominador);
+        }
     }
 }
